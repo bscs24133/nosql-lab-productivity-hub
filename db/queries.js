5e18af8e23e32afe12cd41bc140b4a73048aeed1
bscs24133 <bscs24133@itu.edu.pk>
2026-04-27 13:08:54 +0500

    querie 15 recent activity feed!

--- db/queries.js
+++ db/queries.js
@@ -488,7 +488,40 @@ async function projectTaskSummary(db, ownerId) {
  *       you only want to look up 10 projects, not all of them.
  */
 async function recentActivityFeed(db, ownerId) {
-  
+  const result = await db.collection('tasks').aggregate([
+    {
+      $match: { ownerId: ownerId }
+    },
+    {
+      $sort: { createdAt: -1 }
+    },
+    {
+      $limit: 10
+    },
+    {
+      $lookup: {
+        from: 'projects',
+        localField: 'projectId',
+        foreignField: '_id',
+        as: 'project'
+      }
+    },
+    {
+      $unwind: '$project'
+    },
+    {
+      $project: {
+        _id: 1,
+        title: 1,
+        status: 1,
+        priority: 1,
+        createdAt: 1,
+        projectId: 1,
+        projectName: '$project.name'
+      }
+    }
+  ]).toArray();
+  return result;
   //throw new Error('recentActivityFeed not implemented');
 }
 
